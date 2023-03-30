0355cf0d3e73db3e1e7e94553e26d9f95359895a
firasel <mdrasel601390@gmail.com>
2023-03-31 01:31:12 +0600

    Add tailwindCSS

--- src/lib/components/home/Home.svelte
+++ src/lib/components/home/Home.svelte
@@ -1,3 +1,3 @@
 <div>
-	<h2>Hello World 2</h2>
+	<h2 class="text-center text-2xl text-orange-600">Hello World 2</h2>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
+<script>
+	import Home from '$lib/components/home/Home.svelte';
+</script>
+
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h2>Hello world</h2>
+	<h1 class="text-center text-5xl my-10 text-gray-700">Hello world</h1>
+	<Home />
 </section>
