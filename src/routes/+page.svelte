650354d40b3b4bcfdf5d566ea5c02f1e01663a79
firasel <mdrasel601390@gmail.com>
2023-04-02 23:33:26 +0600

    Rename the component name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script>
-	import Home from '$lib/components/home/Home.svelte';
+	import Hero from '$lib/components/hero/Hero.svelte';
+	import Timeline from '$lib/components/timeline/Timeline.svelte';
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-<main class="bg-gradient-1">
-	<Home />
+<main>
+	<div class="bg-gradient-1">
+		<Hero />
+	</div>
+	<div>
+		<Timeline />
+	</div>
 </main>
